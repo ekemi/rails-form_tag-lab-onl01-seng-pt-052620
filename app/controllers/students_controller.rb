5eb437abee6e3c7a7bcdcc337c76788a711bc11f
edmundo papi <edmundopierre@yahoo.com>
2020-12-26 03:19:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -25,7 +25,7 @@ def index
   end
 
   def create
-    @student =  Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student =  Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to student_path(@student)
   end
 
